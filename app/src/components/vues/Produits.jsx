0086cbb541bd1fa803a4d0abff041d9b2892b92c
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-07-15 11:52:02 +0200

    init tailwind, accueuil et produits

---
+++ app/src/components/vues/Produits.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Produits = () => {
+  return (
+    <div>
+      <h1>Produits</h1>
+      <p>Voici la liste de nos produits.</p>
+    </div>
+  );
+};
+
+export default Produits;
