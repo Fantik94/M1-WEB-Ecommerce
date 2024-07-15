97dea9d55fb2576050c9c6b28ea33cd3c6c17b71
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-07-15 20:58:42 +0200

    chore: Update animation duration for marquee to improve user experience

---
+++ app/src/components/vues/Comment.jsx
@@ -0,0 +1,61 @@
+// src/components/vues/Comment.jsx
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Comment = ({ name, dateJoined, rating, title, reviewDate, reviewText, helpfulCount }) => {
+  const stars = Array.from({ length: 5 }, (_, i) => (
+    <svg
+      key={i}
+      className={`w-4 h-4 ${i < rating ? 'text-yellow-300' : 'text-gray-300'}`}
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 22 20"
+    >
+      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+  ));
+
+  return (
+    <article className="p-6 mb-6 text-base bg-white border border-gray-200 rounded-lg shadow-md">
+      <div className="flex items-center mb-4">
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full">
+          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
+          </svg>
+        </div>
+        <div className="font-medium text-gray-900">
+          <p>
+            {name} <time className="block text-sm text-gray-500">A rejoint en {dateJoined}</time>
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center mb-1 space-x-1">{stars}</div>
+      <h3 className="mt-2 text-sm font-semibold text-gray-900">{title}</h3>
+      <footer className="mb-5 text-sm text-gray-500">
+        <p>Commenté en France le <time>{reviewDate}</time></p>
+      </footer>
+      <p className="mb-2 text-gray-500">{reviewText}</p>
+      <a href="#" className="block mb-5 text-sm font-medium text-blue-600 hover:underline">Lire plus</a>
+      <aside>
+        <p className="mt-1 text-xs text-gray-500">{helpfulCount} personnes ont trouvé cela utile</p>
+        <div className="flex items-center mt-3">
+          <a href="#" className="px-2 py-1.5 text-xs font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-100">Utile</a>
+          <a href="#" className="pl-4 text-sm font-medium text-blue-600 hover:underline ml-4 border-gray-200 border-l">Signaler un abus</a>
+        </div>
+      </aside>
+    </article>
+  );
+};
+
+Comment.propTypes = {
+  name: PropTypes.string.isRequired,
+  dateJoined: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  reviewDate: PropTypes.string.isRequired,
+  reviewText: PropTypes.string.isRequired,
+  helpfulCount: PropTypes.number.isRequired,
+};
+
+export default Comment;
--- app/src/components/vues/Home.jsx
+++ app/src/components/vues/Home.jsx
@@ -1,14 +1,78 @@
 // src/components/vues/Home.jsx
 import React from 'react';
 import Banner from './Banner';
+import Comment from './Comment';
 
 const Home = () => {
   return (
     <div>
       <Banner text="L'espace inspiré de gamer pour les gamer." image="../../banner.webp" />
       <div className="p-4">
-        <h1 className="text-2xl font-bold">Accueil</h1>
-        <p>Bienvenue sur notre page d'accueil.</p>
+        <h1 className="text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            Bienvenue dans le paradis du gamer
+          </span>
+        </h1>
+        <p className="mt-4 text-lg font-normal text-gray-500 lg:text-xl">
+          Chez Gaming Avenue, nous comprenons les besoins des gamers. Que vous soyez à la recherche du dernier PC gaming, des périphériques les plus performants, ou des snacks et boissons pour vos sessions de jeu marathons, nous avons tout ce qu'il vous faut. Découvrez notre sélection et faites de votre espace de jeu un véritable paradis.
+        </p>
+
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="relative">
+            <img src="/setup-gaming.jpg" alt="Gaming Setup" className="w-full h-full object-cover rounded-lg shadow-lg" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-lg">
+              <h2 className="text-white text-2xl font-bold">Setups Gaming</h2>
+            </div>
+          </div>
+          <div className="relative">
+            <img src="/pc.jpg" alt="PC Gaming" className="w-full h-full object-cover rounded-lg shadow-lg" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-lg">
+              <h2 className="text-white text-2xl font-bold">PC Gaming</h2>
+            </div>
+          </div>
+          <div className="relative">
+            <img src="/snack.webp" alt="Gaming Snacks" className="w-full h-full object-cover rounded-lg shadow-lg" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-lg">
+              <h2 className="text-white text-2xl font-bold">Snacks & Boissons</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-12">
+          <h2 className="text-2xl font-extrabold text-gray-900 md:text-3xl lg:text-4xl">Témoignages</h2>
+          <p className="mt-4 text-lg font-normal text-gray-500 lg:text-xl">
+            Voici ce que disent nos clients :
+          </p>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <Comment
+              name="Jese Leos"
+              dateJoined="Août 2014"
+              rating={4}
+              title="Je pense en acheter un autre !"
+              reviewDate="3 mars 2017"
+              reviewText="J'adore Hitema, c'est faux c trop de la merde"
+              helpfulCount={19}
+            />
+            <Comment
+              name="Julie Smith"
+              dateJoined="Janvier 2018"
+              rating={5}
+              title="Parfait pour les longues sessions de jeu !"
+              reviewDate="10 mai 2018"
+              reviewText="Les snacks et boissons disponibles sont parfaits pour mes sessions de jeu marathon. Je recommande vivement !"
+              helpfulCount={25}
+            />
+            <Comment
+              name="Max Doe"
+              dateJoined="Juin 2016"
+              rating={5}
+              title="Meilleur magasin de jeux !"
+              reviewDate="2 octobre 2019"
+              reviewText="Le meilleur endroit pour tous les gamers. Un large choix de produits et un service client au top !"
+              helpfulCount={30}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
