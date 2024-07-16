80046165f080c5ac19616c165ad32e38233e3c4e
Damien <77857194+dada919@users.noreply.github.com>
2024-07-16 16:21:05 +0200

    Modification de livraison

--- app/src/components/vues/Livraison.jsx
+++ app/src/components/vues/Livraison.jsx
@@ -66,10 +66,9 @@ const Livraison = () => {
               <button type="submit" className="w-full py-3 px-6 text-sm font-semibold rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Suivant</button>
             </form>
           </div>
-        </div>
-      </div>
 
-      <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
+          
+          <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
             <div className="space-y-4 rounded-lg border border-gray-300 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
               <p className="text-xl font-semibold text-gray-900 dark:text-white">Récapitulatif de la commande</p>
 
@@ -114,6 +113,8 @@ const Livraison = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
     </section>
   );
 };
