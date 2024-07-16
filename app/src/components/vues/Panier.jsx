4068fb1021f528692a583be59bafd0195682b0a8
Damien <77857194+dada919@users.noreply.github.com>
2024-07-16 13:35:03 +0200

    modification de la page panier

--- app/src/components/vues/Panier.jsx
+++ app/src/components/vues/Panier.jsx
@@ -37,7 +37,7 @@ const Panier = () => {
               {panier.length === 0 ? (
                 <div className="rounded-lg border border-gray-300 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
                   <center>
-                    <p className="text-base font-medium text-gray-900 hover:underline dark:text-white">Votre panier est vide. ☹️</p>
+                    <p className="text-base font-medium text-gray-900 dark:text-white">Votre panier est vide. ☹️</p>
                     <br />
                     <Link to="/recherche" className="flex w-[200px] items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                       Voir notre catalogue
@@ -46,49 +46,50 @@ const Panier = () => {
                 </div>
               ) : null}
 
-              <div className="rounded-lg border border-gray-300 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
-                <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
-                  <a href="#" className="shrink-0 md:order-1">
-                    <img className="h-20 w-20 dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="imac image" />
-                    <img className="hidden h-20 w-20 dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="imac image" />
-                  </a>
+            <div className="space-y-4">
+                  {panier.map(produit => (
+                    <div key={produit.product_id} className="rounded-lg border border-gray-300 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                      <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                        <Link to={`/produit/${produit.product_id}`} className="shrink-0 md:order-1">
+                          <img className="h-20 w-20 dark:hidden" src={`/images/${produit.product_id}-1.jpg`} alt={produit.name} />
+                          <img className="hidden h-20 w-20 dark:block" src={`/images/${produit.product_id}-1.jpg`} alt={produit.name} />
+                        </Link>
 
-                  <label htmlFor="counter-input" className="sr-only">Choose quantity:</label>
-                  <div className="flex items-center justify-between md:order-3 md:justify-end">
-                    <div className="flex items-center">
-                      <button type="button" id="decrement-button" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                        <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
-                        </svg>
-                      </button>
-                      <input type="text" id="counter-input" className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white" value="2" readOnly />
-                      <button type="button" id="increment-button" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                        <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                        </svg>
-                      </button>
+                        <label htmlFor="counter-input" className="sr-only">Choose quantity:</label>
+                        <div className="flex items-center justify-between md:order-3 md:justify-end">
+                          <div className="flex items-center">
+                            <button type="button" onClick={() => retirer(produit)} className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                              <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
+                              </svg>
+                            </button>
+                            <input type="text" id="counter-input" className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white" value={produit.quantite} readOnly />
+                            <button type="button" onClick={() => ajouter(produit)} className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                              <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
+                              </svg>
+                            </button>
+                          </div>
+                          <div className="text-end md:order-4 md:w-32">
+                            <p className="text-base font-bold text-gray-900 dark:text-white">{produit.price * produit.quantite}€</p>
+                          </div>
+                        </div>
+
+                        <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                          <Link to={`/produit/${produit.product_id}`} className="text-base font-medium text-gray-900 hover:underline dark:text-white">{produit.name}</Link>
+                          <div className="flex items-center gap-4">
+                            <button type="button" onClick={() => retirer(produit)} className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500">
+                              <svg className="mr-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
+                              </svg>
+                              Supprimer
+                            </button>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className="text-end md:order-4 md:w-32">
-                      <p className="text-base font-bold text-gray-900 dark:text-white">$1,499</p>
-                    </div>
-                  </div>
-
-                  <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
-                    <a href="#" className="text-base font-medium text-gray-900 hover:underline dark:text-white">PC system All in One APPLE iMac (2023) mqrq3ro/a, Apple M3, 24" Retina 4.5K, 8GB, SSD 256GB, 10-core GPU, Keyboard layout INT</a>
-
-                    <div className="flex items-center gap-4">
-                     
-
-                      <button type="button" className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500">
-                        <svg className="mr-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
-                        </svg>
-                        Supprimer
-                      </button>
-                    </div>
-                  </div>
+                  ))}
                 </div>
-              </div>
               {/* Add more products similarly */}
             </div>
           </div>
