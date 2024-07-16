9051836853d4c8a20bb980c72f159d823d5afc51
Damien <77857194+dada919@users.noreply.github.com>
2024-07-16 11:53:34 +0200

    Ajout du context Panier

--- app/src/components/vues/Produits.jsx
+++ app/src/components/vues/Produits.jsx
@@ -80,6 +80,22 @@ const Product = () => {
             </div>
 
             <div className="flex flex-wrap gap-4 mt-8">
+              <div className="flex items-center">
+                      <button type="button" id="decrement-button" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                        <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
+                        </svg>
+                      </button>
+                      <input type="text" id="counter-input" className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white" value="2" readOnly />
+                      <button type="button" id="increment-button" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                        <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
+                        </svg>
+                      </button>
+                    </div>
+            </div>
+
+            <div className="flex flex-wrap gap-4 mt-8">
               <h4 className={`text-base ${product.stock < 1 ? 'text-red-600' : 'text-gray-800'}`}>
                 Stock : {product.stock < 1 ? 0 : product.stock}
               </h4>
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
+import { PanierContextProvider } from './context/PanierContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-    <App />
+      <PanierContextProvider>
+        <App />
+      </PanierContextProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
