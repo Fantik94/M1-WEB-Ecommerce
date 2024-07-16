77ced166ab663bd516982d3c34529cb43369849f
Damien <77857194+dada919@users.noreply.github.com>
2024-07-16 11:08:10 +0200

    modification de la page produit

--- app/src/components/vues/Produits.jsx
+++ app/src/components/vues/Produits.jsx
@@ -7,16 +7,22 @@ const Product = () => {
   const { productId } = useParams();
   const [product, setProduct] = useState(null);
   const [similarProducts, setSimilarProducts] = useState([]);
+  const [mainImage, setMainImage] = useState(null);
 
   useEffect(() => {
     axios.get(`http://localhost:3000/products/${productId}`)
       .then(response => {
         setProduct(response.data);
+        setMainImage(`/images/${response.data.product_id}-1.jpg`);
       })
       .catch(error => {
         console.error(`There was an error fetching the product ${productId}:`, error);
       });
   }, [productId]);
+
+  const handleImageClick = (num) => {
+    setMainImage(`/images/${product.product_id}-${num}.jpg`);
+  };
 
   if (!product) {
     return <div>Loading...</div>;
@@ -28,12 +34,16 @@ const Product = () => {
         <div className="grid items-start grid-cols-1 lg:grid-cols-5 gap-12 shadow-md p-6 rounded-lg">
           <div className="lg:col-span-3 w-full lg:sticky top-0 text-center">
             <div className="px-4 py-10 rounded-lg shadow-md relative">
-              <img src={`/images/${product.product_id}-1.jpg`} alt={product.name} className="w-3/4 rounded object-cover mx-auto" />
+              <img src={mainImage} alt={product.name} className="w-3/4 rounded object-cover mx-auto" />
             </div>
 
             <div className="mt-6 flex flex-wrap justify-center gap-6 mx-auto">
               {['1', '2', '3'].map(num => (
-                <div key={num} className="w-24 h-20 flex items-center justify-center rounded-lg p-4 shadow-md cursor-pointer">
+                <div
+                  key={num}
+                  className="w-24 h-20 flex items-center justify-center rounded-lg p-4 shadow-md cursor-pointer"
+                  onClick={() => handleImageClick(num)}
+                >
                   <img src={`/images/${product.product_id}-${num}.jpg`} alt={`${product.name} ${num}`} className="w-full" />
                 </div>
               ))}
@@ -45,7 +55,7 @@ const Product = () => {
 
             <div className="flex space-x-2 mt-4">
               {[...Array(4)].map((_, i) => (
-                <svg key={i} className="w-5 fill-blue-600" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg key={i} className="w-5 fill-primary-700" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
                 </svg>
               ))}
@@ -66,15 +76,26 @@ const Product = () => {
             </div>
 
             <div className="flex flex-wrap gap-4 mt-8">
-              <button type="button" className="min-w-[200px] px-4 py-3 bg-blue-600 hover:bg-blue-700 text-white text-sm font-semibold rounded">Acheter maintenant</button>
-              <button type="button" className="min-w-[200px] px-4 py-2.5 border border-blue-600 bg-transparent hover:bg-gray-50 text-gray-800 text-sm font-semibold rounded">Ajouter au panier</button>
+              <button
+                type="button"
+                className={`min-w-[200px] px-4 py-3 ${product.stock < 1 ? 'bg-gray-400 cursor-not-allowed' : 'bg-primary-700 hover:bg-blue-700'} text-white text-sm font-semibold rounded`}
+                disabled={product.stock < 1}
+              >
+                Acheter maintenant
+              </button>
+              <button
+                type="button"
+                className={`min-w-[200px] px-4 py-2.5 ${product.stock < 1 ? 'border-gray-400 bg-gray-200 text-gray-400 cursor-not-allowed' : 'border-primary-700 bg-transparent hover:bg-gray-50 text-primary-700'} border text-sm font-semibold rounded`}
+                disabled={product.stock < 1}
+              >
+                Ajouter au panier
+              </button>
             </div>
           </div>
         </div>
 
         <div className="mt-16 shadow-md p-6">
           <h3 className="text-xl font-bold text-gray-800">Description du produit</h3>
-          &nbsp;
           <p className="text-gray-800 text-base">{product.description}</p>
         </div>
 
